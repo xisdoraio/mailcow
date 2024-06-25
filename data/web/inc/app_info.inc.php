4570c59d817d7d0141f8178340be0f06308c33cb
joão Carlos Brandão <jcb.bozo@gmail.com>
2024-06-25 09:17:40 +0000

    agora vai

--- data/web/inc/app_info.inc.php
+++ data/web/inc/app_info.inc.php
@@ -1,11 +1,11 @@
 <?php
-  $MAILCOW_GIT_VERSION="";
+  $MAILCOW_GIT_VERSION="4c23030";
   $MAILCOW_LAST_GIT_VERSION="";
   $MAILCOW_GIT_OWNER="mailcow";
   $MAILCOW_GIT_REPO="mailcow-dockerized";
   $MAILCOW_GIT_URL="https://github.com/mailcow/mailcow-dockerized";
-  $MAILCOW_GIT_COMMIT="origin/master";
-  $MAILCOW_GIT_COMMIT_DATE="2024-05-15 11:35:26 -0300";
-  $MAILCOW_BRANCH="master";
-  $MAILCOW_UPDATEDAT=1716148031;
+  $MAILCOW_GIT_COMMIT="origin/nightly";
+  $MAILCOW_GIT_COMMIT_DATE="2024-05-19 19:49:09 +0000";
+  $MAILCOW_BRANCH="nightly";
+  $MAILCOW_UPDATEDAT=1719306595;
 ?>
